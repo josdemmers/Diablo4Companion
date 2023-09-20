1019136b893359a9fef3668d256aad3e108f9cbd
Jos Demmers <josdemmers@gmail.com>
2023-09-20 18:11:59 +0200

    Updated system preset: 1080p_SMF_en.zip

---
+++ D4Companion.Entities/SigilInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4Companion.Entities
+{
+    public class SigilInfo
+    {
+        public int IdSno { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+    }
+}

11c9c5dba773cbc324175eddbd45da87769aa8e3
Jos Demmers <josdemmers@gmail.com>
2023-12-02 22:05:52 +0100

    Text of used affixes/aspects that are missing are now marked red.

---
+++ D4Companion/Converters/AspectIdToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using D4Companion.Interfaces;
+using D4Companion.Services;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace D4Companion.Converters
+{
+    [ValueConversion(typeof(string), typeof(SolidColorBrush))]
+    public class AspectIdToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return string.Empty;
+
+            var systemPresetManager = (ISystemPresetManager)Prism.Ioc.ContainerLocator.Container.Resolve(typeof(ISystemPresetManager));
+
+            var isMappingReady = systemPresetManager.AffixMappings.Any(mapping => mapping.IdName.Equals((string)value));
+            return isMappingReady ?
+                new SolidColorBrush(Color.FromRgb(System.Drawing.Color.LightGray.R, System.Drawing.Color.LightGray.G, System.Drawing.Color.LightGray.B)) :
+                new SolidColorBrush(Color.FromRgb(System.Drawing.Color.Red.R, System.Drawing.Color.Red.G, System.Drawing.Color.Red.B));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

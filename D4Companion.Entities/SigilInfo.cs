225aa5a7367fc87100fc6231583e41334b2e261f
Jos Demmers <josdemmers@gmail.com>
2024-03-26 17:29:56 +0100

    Updated game data

--- D4Companion.Entities/SigilInfo.cs
+++ D4Companion.Entities/SigilInfo.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace D4Companion.Entities
+﻿namespace D4Companion.Entities
 {
     public class SigilInfo
     {
-        public int IdSno { get; set; }
+        public long IdSno { get; set; }
         public string IdName { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string DungeonZoneInfo { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
     }
 }

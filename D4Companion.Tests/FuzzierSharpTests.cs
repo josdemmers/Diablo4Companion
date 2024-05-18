ab6ea0556f9d65ed7fc2ee844c961c7e2c5d9940
Jos Demmers <josdemmers@gmail.com>
2024-05-18 09:51:01 +0200

    System preset update: 1440p_SSF_en.zip Added Chinese OCR tests

--- D4Companion.Tests/FuzzierSharpTests.cs
+++ D4Companion.Tests/FuzzierSharpTests.cs
@@ -1,4 +1,4 @@
-using D4Companion.Entities;
+﻿using D4Companion.Entities;
 using D4Companion.Helpers;
 using D4Companion.Interfaces;
 using D4Companion.Services;
@@ -12,23 +12,30 @@ namespace D4Companion.Tests
 {
     public class FuzzierSharpTests
     {
+        // enUS
         private List<AffixInfo> _affixes = new List<AffixInfo>();
         private List<SigilInfo> _sigils = new List<SigilInfo>();
         private List<string> _affixDescriptions = new List<string>();
         private List<string> _sigilNames = new List<string>();
         private Dictionary<string, string> _affixMapDescriptionToId = new Dictionary<string, string>();
         private Dictionary<string, string> _sigilMapNameToId = new Dictionary<string, string>();
-        /// <summary>
-        /// Input text, expected affix id
-        /// </summary>
         private Dictionary<string, string> _affixTestMappings = new Dictionary<string, string>();
         private Dictionary<string, string> _sigilTestMappings = new Dictionary<string, string>();
+
+        // zhCN
+        private List<AffixInfo> _affixeszhCN = new List<AffixInfo>();
+        private List<string> _affixDescriptionszhCN = new List<string>();
+        private Dictionary<string, string> _affixMapDescriptionToIdzhCN = new Dictionary<string, string>();
+        private Dictionary<string, string> _affixTestMappingszhCN = new Dictionary<string, string>();
+
+
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
             // Init data
             InitAffixData();
+            InitAffixDatazhCN();
             InitSigilData();
             InitTestData();
         }
@@ -57,6 +64,21 @@ namespace D4Companion.Tests
                     var sigilId = _sigilMapNameToId[result.Value];
 
                     Assert.That(sigilId, Is.EqualTo(sigilTest.Value), $"Input: {sigilTest.Key}");
+                }
+            });
+        }
+
+        [Test]
+        public void DefaultRatioScorerTestzhCN()
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var affixTest in _affixTestMappingszhCN)
+                {
+                    var result = Process.ExtractOne(affixTest.Key, _affixDescriptionszhCN, processor: (s) => s, scorer: ScorerCache.Get<DefaultRatioScorer>());
+                    var affixId = _affixMapDescriptionToIdzhCN[result.Value];
+
+                    Assert.That(affixId, Is.EqualTo(affixTest.Value), $"Input: {affixTest.Key}");
                 }
             });
         }
@@ -275,6 +297,36 @@ namespace D4Companion.Tests
             _affixMapDescriptionToId = _affixes.ToDictionary(affix => affix.DescriptionClean, affix => affix.IdName);
         }
 
+        private void InitAffixDatazhCN()
+        {
+            _affixeszhCN.Clear();
+            string resourcePath = @$".\Data\Affixes.zhCN.json";
+            using (FileStream? stream = File.OpenRead(resourcePath))
+            {
+                if (stream != null)
+                {
+                    // create the options
+                    var options = new JsonSerializerOptions()
+                    {
+                        WriteIndented = true
+                    };
+                    // register the converter
+                    options.Converters.Add(new BoolConverter());
+                    options.Converters.Add(new IntConverter());
+
+                    _affixeszhCN = JsonSerializer.Deserialize<List<AffixInfo>>(stream, options) ?? new List<AffixInfo>();
+                }
+            }
+
+            // Create affix description list for FuzzierSharp
+            _affixDescriptionszhCN.Clear();
+            _affixDescriptionszhCN = _affixeszhCN.Select(affix => affix.DescriptionClean).ToList();
+
+            // Create dictionary to map affix description with affix id
+            _affixMapDescriptionToIdzhCN.Clear();
+            _affixMapDescriptionToIdzhCN = _affixeszhCN.ToDictionary(affix => affix.DescriptionClean, affix => affix.IdName);
+        }
+
         private void InitSigilData()
         {
             _sigils.Clear();
@@ -307,21 +359,37 @@ namespace D4Companion.Tests
 
         private void InitTestData()
         {
+            InitTestDataenUS();
+            InitTestDatazhCN();
+        }
+
+        private void InitTestDataenUS()
+        {
             // Key: Input text, Value: expected affix id
             _affixTestMappings = new Dictionary<string, string>
             {
-                {"+715 Thorns","Thorns"},
-                {"+715 Thoms","Thorns"},
-                {"18 All Stats","CoreStats_All"},
-                {"+10% Damage","Damage"},
-                {"+19.5% Shadow Damage Over Time","Damage_Type_DoT_Bonus_Shadow"},
-                {"+19.5% Fire Damage Over Time", "Damage_Type_DoT_Bonus_Burn"},
-                {"4.5% Damage Reduction [3.1 - 7.6]%","DamageReduction"},
-                {"16.7% Damage Reduction from Distant Enemies [10.3 - 17.4]%","DamageReductionDistant"}
+                {"+715 Thorns","Tempered_Generic_Thorns_Tier3"},
+                {"+715 Thoms","Tempered_Generic_Thorns_Tier3"},
+                {"18 All Stats","S04_CoreStats_All"},
+                {"+10% Damage","Tempered_Damage_Generic_All_Tier3"},
+                {"+19.5% Shadow Damage Over Time","Tempered_Damage_Necro_DoT_Shadow_Tier3"},
+                {"+19.5% Fire Damage Over Time", "Tempered_Damage_Sorc_DoT_Burn_Tier3"},
+                {"4.5% Damage Reduction [3.1 - 7.6]%","S04_DamageReduction"}
             };
             _sigilTestMappings = new Dictionary<string, string>
             {
                 {"monster cold resist","DungeonAffix_Minor_Monster_LessCold"}
+            };
+        }
+
+        private void InitTestDatazhCN()
+        {
+            // Key: Input text, Value: expected affix id
+            _affixTestMappingszhCN = new Dictionary<string, string>
+            {
+                {"# 点全属性","S04_CoreStats_All"},
+                {"+79 点智力","S04_CoreStat_Intelligence"},
+                {"幸运一击：最多有 +#% 几率冻结，持续 2 秒","Tempered_Generic_LuckyHit_Freeze_Tier3"},
             };
         }
     }
